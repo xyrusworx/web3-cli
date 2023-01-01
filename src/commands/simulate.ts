22a83df4e2ca97d6e9d232938eca925d7d05568a
xyrus02 <xyrus01@gmail.com>
2023-01-01 12:35:04 +0100

    Added support for STDIN on simulator to provide data

--- src/commands/simulate.ts
+++ src/commands/simulate.ts
@@ -25,10 +25,16 @@ export default class SimulateCommand extends SimulatorCommand {
             return exit || 0;
 
         let from: string, value: string, block: number;
+        let fromStdin: boolean = false;
 
         for(let i = 0; i < args.length; i++) {
             const arg = args[i];
             switch (arg) {
+                case "-i":
+                case "--stdin":
+                    fromStdin = true;
+                    break;
+
                 case "-u":
                 case "--from":
                     from = args[++i];
@@ -58,14 +64,27 @@ export default class SimulateCommand extends SimulatorCommand {
             }
         }
 
-        const to = args[args.length - 2];
+        const to = args[args.length - (fromStdin ? 1 : 2)];
         if (!to || !to.match(/^(0[xX])?[a-fA-F0-9]{40}$/)) {
             console.error("Invalid target address:", to);
             return 1;
         }
 
-        const data = args[args.length - 1];
-        if (!data || !data.match(/^(0[xX])?[a-fA-F0-9]+$/)) {
+        async function read(stream) {
+            return await new Promise<string>(resolve => {
+                const chunks = [];
+                stream.on("data", chunk => chunks.push(chunk));
+                stream.on("end", () => {
+                    resolve(Buffer.concat(chunks).toString('utf8').trim());
+                });
+            });
+        }
+
+        let data: string = fromStdin
+            ? await read(process.stdin)
+            : args[args.length - 1];
+
+        if (!data || !data.match(/^(0[xX])?[a-fA-F0-9]*$/)) {
             console.error("Invalid ABI-encoded data:", data);
             return 1;
         }
@@ -87,7 +106,7 @@ export default class SimulateCommand extends SimulatorCommand {
     private usage() {
         const console = this.output;
 
-        console.log("Usage: web3 simulate [options] <target address> <data>");
+        console.log("Usage: web3 simulate [options] <target address> [<data>]");
         console.log("Available options:");
 
         commonHelp(console);
@@ -98,8 +117,11 @@ export default class SimulateCommand extends SimulatorCommand {
         console.log("  --value / -v <number>  Uses the given value as the transaction value.");
         console.log("                         Must be in native currency. Hex values are supported.")
         console.log("  --from / -u <address>  Sets the sender address for the transaction.");
+        console.log("  --stdin / -i           If set, the ABI-encoded data is read from STDIN.");
+        console.log("                         This is useful in combination with the \"encode\" command.")
         console.log("");
-        console.log("The data must be ABI-encoded data in hex representation.");
+        console.log("The data must be ABI-encoded data in hex representation. Unless --stdin / -i is set, it");
+        console.log("is mandatory to specify the ABI-encoded data. If you want to send empty data, specify \"0x\".")
 
         console.log("");
         evmNetworkHelp(console);
